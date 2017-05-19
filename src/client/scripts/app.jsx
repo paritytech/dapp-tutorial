177e2bc31d21442135c06f4de9d9c904396306f0
Gav <gavin@parity.io>
2017-05-19 10:59:49 +0200

    Tutorial 6.1

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -4,29 +4,32 @@ import {Rspan, Rimg} from 'oo7-react';
 import {InputBond, HashBond, BButton, TransactButton} from 'parity-reactive-ui';
 import {formatBalance, isNullData} from 'oo7-parity';
 
+const CounterABI = [
+	{"constant":false,"inputs":[{"name":"_option","type":"uint256"}],"name":"vote","outputs":[],"payable":false,"type":"function"},
+	{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"hasVoted","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},
+	{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"votes","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},
+	{"anonymous":false,"inputs":[{"indexed":true,"name":"who","type":"address"},{"indexed":true,"name":"option","type":"uint256"}],"name":"Voted","type":"event"}
+];
+const Options = ['Red', 'Green', 'Blue'];
+
 export class App extends React.Component {
 	constructor() {
 		super();
-		this.name = new Bond;
-		this.recipient = parity.bonds.registry.lookupAddress(this.name, 'A');
+		this.counter = parity.bonds.makeContract('0x7aC77Cb854E064f22E747F40b90FE6D6Bc1e3197', CounterABI);
 	}
-	render() {
-		return (
-			<div>
-				My balance: <Rspan>
-					{parity.bonds.balance(parity.bonds.me).map(formatBalance)}
+	render () {
+		return (<div>
+			{Options.map((n, i) => (<div key={i}>
+				<Rspan style={{
+					borderLeft: this.counter
+						.votes(i)
+						.map(v => `${1 + v * 10}px black solid`)
+				}}>
+					<span style={{float: 'left', minWidth: '3em'}}>
+						{n}
+					</span>
 				</Rspan>
-				<br />
-				<InputBond bond={this.name} placeholder='Name of recipient' />
-				<TransactButton
-					content={this.name.map(n => `Give ${n} 100 Finney`)}
-					disabled={this.recipient.map(isNullData)}
-					tx={{
-						to: this.recipient,
-						value: 100 * 1e15
-					}}
-				/>
-			</div>
-		);
+			</div>))}
+		</div>);
 	}
 }

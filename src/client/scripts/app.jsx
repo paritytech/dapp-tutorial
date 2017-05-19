cc3b903e7ec9557481782ec1f2e08d28d702b905
Gav <gavin@parity.io>
2017-05-19 10:15:37 +0200

    Tutorial 3.1

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -2,27 +2,16 @@ import React from 'react';
 import {Bond, TimeBond} from 'oo7';
 import {Rspan} from 'oo7-react';
 import {InputBond} from 'parity-reactive-ui';
-
-const computeColor = t => t.match(/^[0-9]+$/) ? {color: 'red'} : {color: 'black'}
-const format = ([msg, t]) => `${new Date(t)}: ${msg}`
+import {formatBlockNumber} from 'oo7-parity';
 
 export class App extends React.Component {
-	constructor() {
-		super();
-		this.bond = new Bond();
-		this.time = new TimeBond();
-	}
 	render() {
 		return (
 			<div>
-				<InputBond
-					bond={this.bond}
-					placeholder="Go ahead and type some text"
-				/>
-				<Rspan
-					style={this.bond.map(computeColor)}
-				>
-					{Bond.all([this.bond, this.time]).map(format)}
+				Current block is:
+				&nbsp;
+				<Rspan style={{fontWeight: 'bold'}}>
+					{parity.bonds.height.map(formatBlockNumber)}
 				</Rspan>
 			</div>
 		);

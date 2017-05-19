c8c0218908788fb3db059c10ee99f9a4462347d8
Gav <gavin@parity.io>
2017-05-19 11:33:39 +0200

    Tutorial 9.2

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -10,6 +10,18 @@ const CounterABI = [
 	{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"votes","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},
 	{"anonymous":false,"inputs":[{"indexed":true,"name":"who","type":"address"},{"indexed":true,"name":"option","type":"uint256"}],"name":"Voted","type":"event"}
 ];
+const CounterCode = '\
+0x6060604052341561000c57fe5b5b61017d8061001c6000396000f300606060405263fffff\
+fff60e060020a6000350416630121b93f811461003757806309eef43e1461004c5780635df8\
+13301461007c575bfe5b341561003f57fe5b61004a6004356100a1565b005b341561005457f\
+e5b610068600160a060020a036004351661012a565b60408051911515825251908190036020\
+0190f35b341561008457fe5b61008f60043561013f565b60408051918252519081900360200\
+190f35b600160a060020a03331660009081526001602052604090205460ff16156100c85760\
+006000fd5b60008181526020818152604080832080546001908101909155600160a060020a0\
+3331680855292819052818420805460ff191690911790555183927f4d99b957a2bc29a30ebd\
+96a7be8e68fe50a3c701db28a91436490b7d53870ca491a35b50565b6001602052600090815\
+2604090205460ff1681565b600060208190529081526040902054815600a165627a7a723058\
+20ef4d0d5cd232790fab92fcd67c633d228600212c1f337d0c68622f19f106c7fc0029';
 const Options = ['Red', 'Green', 'Blue'];
 
 class VoteOption extends ReactiveComponent {
@@ -72,11 +84,23 @@ export class Counter extends React.Component {
 export class App extends React.Component {
 	constructor () {
 		super();
-		this.state = { counter: parity.bonds.makeContract('0x7aC77Cb854E064f22E747F40b90FE6D6Bc1e3197', CounterABI) };
+		this.state = { tx: null, counter: null };
+		this.deploy = this.deploy.bind(this);
+	}
+	deploy () {
+		let tx = parity.bonds.deployContract(CounterCode, CounterABI);
+		this.setState({tx});
+		tx.done(s => this.setState({ counter: s.deployed }));
 	}
 	render () {
 		return (<div>
-			<Counter contract={this.state.counter} />
+			{!!this.state.counter
+				? <Counter contract={this.state.counter} />
+				: <div>
+					<BButton content='Deploy' onClick={this.deploy}/>
+					<TransactionProgressLabel value={this.state.tx}/>
+				</div>
+			}
 		</div>);
 	}
 }

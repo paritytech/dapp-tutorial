dff3f40dd88b131cf4fbf98d42845264508d48bc
Gav <gavin@parity.io>
2017-05-19 10:11:29 +0200

    Tutorial 2.4

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
-import {Bond} from 'oo7';
+import {Bond, TimeBond} from 'oo7';
 import {Rspan} from 'oo7-react';
 import {InputBond} from 'parity-reactive-ui';
+
+const computeColor = t => t.match(/^[0-9]+$/) ? {color: 'red'} : {color: 'black'}
+const format = ([msg, t]) => `${new Date(t)}: ${msg}`
 
 export class App extends React.Component {
 	constructor() {
 		super();
 		this.bond = new Bond();
+		this.time = new TimeBond();
 	}
 	render() {
 		return (
 			<div>
-				<InputBond bond={this.bond} placeholder="Go ahead and type some text"/>
-				<Rspan style={{color: this.bond.map(t => t.match(/^[0-9]+$/) ? 'red' : 'black')}}>
-					{this.bond.map(t => t.toUpperCase())}
+				<InputBond
+					bond={this.bond}
+					placeholder="Go ahead and type some text"
+				/>
+				<Rspan
+					style={this.bond.map(computeColor)}
+				>
+					{Bond.all([this.bond, this.time]).map(format)}
 				</Rspan>
 			</div>
 		);

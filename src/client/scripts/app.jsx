957d75f8343eb7af5f72452c9115984baa3116bb
Gav <gavin@parity.io>
2017-05-19 10:07:55 +0200

    Tutorial 2.3

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -12,7 +12,9 @@ export class App extends React.Component {
 		return (
 			<div>
 				<InputBond bond={this.bond} placeholder="Go ahead and type some text"/>
-				<Rspan>{this.bond.map(t => t.toUpperCase())}</Rspan>
+				<Rspan style={{color: this.bond.map(t => t.match(/^[0-9]+$/) ? 'red' : 'black')}}>
+					{this.bond.map(t => t.toUpperCase())}
+				</Rspan>
 			</div>
 		);
 	}

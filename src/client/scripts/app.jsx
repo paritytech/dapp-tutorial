ef10deb81abe4f66c3a5a0a4e587f6acc5470e63
Gav <gavin@parity.io>
2017-05-19 10:25:18 +0200

    Tutorial 3.4

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import {Bond, TimeBond} from 'oo7';
+import {Bond} from 'oo7';
 import {Rspan} from 'oo7-react';
 import {InputBond} from 'parity-reactive-ui';
-import {formatBlockNumber, formatBalance} from 'oo7-parity';
+import {formatBalance} from 'oo7-parity';
 
 export class App extends React.Component {
 	render() {
 		return (
 			<div>
-				Current block author's balance is:&nbsp;
-				<Rspan style={{fontWeight: 'bold'}}>
-					{parity.bonds.balance(parity.bonds.head.author).map(formatBalance)}
+				Default account:&nbsp;
+				<Rspan>{parity.bonds.me}</Rspan>&nbsp;
+				<br/>Given the name of&nbsp;<Rspan>
+					{parity.bonds.accountsInfo[parity.bonds.me].name}
+				</Rspan>
+				<br/>With a balance of&nbsp;
+				<Rspan>
+					{parity.bonds.balance(parity.bonds.me).map(formatBalance)}
 				</Rspan>
 			</div>
 		);

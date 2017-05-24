0fdd6eb9d48aeeed2d401a07b6bbd8ae5eb5f1ef
Gav <gavin@parity.io>
2017-05-24 07:51:33 -0400

    Minor changes.

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -1,20 +1,9 @@
 import React from 'react';
-import {Rspan} from 'oo7-react';
-import {bonds, ether} from 'oo7-parity';
-import {InlineAccount, TransactButton} from 'parity-reactive-ui';
 
 export class App extends React.Component {
 	render() {
 		return (<div>
-			Block height is <Rspan>{bonds.height}</Rspan>.
-			You are <InlineAccount address={bonds.me}/>.
-			<TransactButton
-				content='Give Gav 1 ether'
-				tx={{
-					to: bonds.registry.lookupAddress('gavofyork', 'A'),
-					value: 1000000000000000000
-				}}
-			/>
+			Hello world!
 		</div>);
 	}
 }

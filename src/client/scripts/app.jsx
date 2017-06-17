5b3db978075f22e154beac218cad09ba6889fe18
Chris Smith <42@chrisryansmith.com>
2017-06-17 17:50:03 -0600

    import bonds and call it directly. Call bond from blocks based on height rather than head

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import {Bond, TimeBond} from 'oo7';
 import {Rspan} from 'oo7-react';
 import {InputBond} from 'parity-reactive-ui';
-import {formatBlockNumber} from 'oo7-parity';
+import {bonds, formatBlockNumber} from 'oo7-parity';
 
 export class App extends React.Component {
 	render() {
 		return (
 			<div>
-				Latest block's timestamp is:&nbsp;
-				<Rspan style={{fontWeight: 'bold'}}>
-					{parity.bonds.head.timestamp.map(_=>_.toString())}
-				</Rspan>
+        Latest block's timestamp is: &nbsp;
+        <Rspan style={{fontWeight: 'bold'}}>
+          {bonds.blocks[bonds.height].map(b => b.timestamp).map(_ => _.toString())}
+        </Rspan>
 			</div>
 		);
 	}

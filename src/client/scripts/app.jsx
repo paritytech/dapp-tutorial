c9c5896beee76023db12e0c4d16593d7cca38768
Chris Smith <42@chrisryansmith.com>
2017-06-17 13:38:02 -0600

    move color to front of declaration to DRY up code

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -12,7 +12,7 @@ export class App extends React.Component {
 		return (
 			<div>
 				<InputBond bond={this.bond} placeholder="Go ahead and type some text"/>
-        <Rspan style={this.bond.map(t => t.match(/^[0-9]+$/) ? {color: 'red'} : {color: 'black'})}>
+        <Rspan style={{color: this.bond.map(t => t.match(/^[0-9]+$/) ? 'red' : 'black')}}>
         	{this.bond.map(t => t.toUpperCase())}
         </Rspan>
 			</div>

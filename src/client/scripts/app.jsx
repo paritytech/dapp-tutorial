a854c5477c42d9b243558f11d88850bbe703c3cf
Chris Smith <42@chrisryansmith.com>
2017-06-17 18:01:41 -0600

    import bonds and use in render

--- src/client/scripts/app.jsx
+++ src/client/scripts/app.jsx
@@ -2,21 +2,24 @@ import React from 'react';
 import {Bond} from 'oo7';
 import {Rspan} from 'oo7-react';
 import {InputBond} from 'parity-reactive-ui';
-import {formatBalance} from 'oo7-parity';
+import {bonds, formatBalance} from 'oo7-parity';
 
 export class App extends React.Component {
 	render() {
 		return (
 			<div>
-				Default account:&nbsp;
-				<Rspan>{parity.bonds.me}</Rspan>&nbsp;
-				<br/>Given the name of&nbsp;<Rspan>
-					{parity.bonds.accountsInfo[parity.bonds.me].name}
-				</Rspan>
-				<br/>With a balance of&nbsp;
-				<Rspan>
-					{parity.bonds.balance(parity.bonds.me).map(formatBalance)}
-				</Rspan>
+        Default account: &nbsp;
+        <Rspan>
+          {bonds.me}
+        </Rspan>
+        <br/>Given the name of&nbsp;
+        <Rspan>
+          {bonds.accountsInfo[bonds.me].name}
+        </Rspan>
+        <br/>With a balance of&nbsp;
+        <Rspan>
+          {bonds.balance(bonds.me).map(formatBalance)}
+        </Rspan>
 			</div>
 		);
 	}
